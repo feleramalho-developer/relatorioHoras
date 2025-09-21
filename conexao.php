031c8f89fc99f4fdb9544df5323b15ca40484b92
Feleramalho <feleramalho@gmail.com>
2025-09-21 19:12:10 -0300

    Publicando-BD-Final2

--- conexao.php
+++ conexao.php
@@ -1,13 +1,15 @@
 <?php
 
-$host = "caboose.proxy.rlwy.net"; 
-$port = 46551; 
-$user = "root"; 
+$host = "caboose.proxy.rlwy.net";
+$port = 46551;
+$user = "root";
 $password = "GXccXsOkyfFEJUBWDwaALivuPWPHwYgP";
-$db = "railway"; 
+$db = "railway";
 
 $mysqli = new mysqli($host, $user, $password, $db, $port);
 
 if ($mysqli->connect_error) {
-    die("Erro na conexão: " . $mysqli->connect_error);
+    die("Falha na conexão: " . $mysqli->connect_error);
 }
+
+?>
